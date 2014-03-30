0dcb13935ec1045681eb75048568faa12a65e6b3
Shubhro Saha <shbhrsaha@gmail.com>
2014-03-30 02:13:32 -0400

    page is up

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
@@ -7,3 +7,6 @@ app.debug = True
 @app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
